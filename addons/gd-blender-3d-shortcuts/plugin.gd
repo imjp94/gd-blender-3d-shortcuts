ba782f5c680411c873950fd072ec5b29ec2d8ec9
Tan Jian Ping <imjp_94@hotmail.com>
2023-06-07 11:00:39 +0800

    Add pie menu

--- addons/gd-blender-3d-shortcuts/plugin.gd
+++ addons/gd-blender-3d-shortcuts/plugin.gd
@@ -2,8 +2,11 @@
 extends EditorPlugin
 
 const Utils = preload("Utils.gd")
+const PieMenuScn = preload("scenes/pie_menu/PieMenu.tscn")
 
 const DEFAULT_LINE_COLOR = Color.WHITE
+
+const items = ["Normal", "Unshaded", "Overdraw", "Wireframe"]
 
 enum SESSION {
 	TRANSLATE,
@@ -19,7 +22,8 @@ var local_space_button
 var snap_button
 var overlay_control
 var spatial_editor_viewports
-
+var pie_menu
+var canvas_layer = CanvasLayer.new()
 
 var overlay_label = Label.new()
 var axis_mesh_inst
@@ -77,6 +81,11 @@ func _ready():
 	local_space_button.connect("toggled", _on_local_space_button_toggled)
 	snap_button = Utils.get_spatial_editor_snap_button(spatial_editor)
 	snap_button.connect("toggled", _on_snap_button_toggled)
+	pie_menu = PieMenuScn.instantiate()
+	pie_menu.theme_source_node = spatial_editor
+	pie_menu.items = items
+	pie_menu.connect("item_focused", _on_PieMenu_item_focused)
+	pie_menu.connect("item_selected", _on_PieMenu_item_selected)
 	var spatial_editor_viewport_container = Utils.get_spatial_editor_viewport_container(spatial_editor)
 	if spatial_editor_viewport_container:
 		spatial_editor_viewports = Utils.get_spatial_editor_viewports(spatial_editor_viewport_container)
@@ -87,8 +96,10 @@ func _input(event):
 		if event.pressed:
 			match event.keycode:
 				KEY_Z:
-					if not (event.ctrl_pressed or event.alt_pressed or event.shift_pressed) and current_session == SESSION.NONE:
-						switch_display_mode()
+					if pie_menu.visible:
+						pie_menu.hide()
+					elif not (event.ctrl_pressed or event.alt_pressed or event.shift_pressed) and current_session == SESSION.NONE:
+							show_pie_menu()
 			# Hacky way to intercept default shortcut behavior when in session
 			if current_session != SESSION.NONE:
 				var event_text = event.as_text()
@@ -122,6 +133,25 @@ func _on_snap_value_changed(text, session):
 			rotate_snap = deg_to_rad(text.to_float())
 		SESSION.SCALE:
 			scale_snap = text.to_float() / 100.0
+
+func _on_PieMenu_item_focused(index):
+	switch_display_mode(index)
+
+func _on_PieMenu_item_selected(index):
+	switch_display_mode(index)
+
+func show_pie_menu():
+	var spatial_editor_viewport = Utils.get_focused_spatial_editor_viewport(spatial_editor_viewports)
+	overlay_control = Utils.get_spatial_editor_viewport_control(spatial_editor_viewport) if spatial_editor_viewport else null
+	if canvas_layer.get_parent() != overlay_control:
+		overlay_control.add_child(canvas_layer)
+	if pie_menu.get_parent() != canvas_layer:
+		canvas_layer.add_child(pie_menu)
+		var viewport = Utils.get_spatial_editor_viewport_viewport(spatial_editor_viewport)
+		pie_menu.select_item(viewport.debug_draw)
+
+	pie_menu.popup(overlay_control.get_global_mouse_position())
+			
 
 func _on_local_space_button_toggled(pressed):
 	is_global = !pressed
@@ -606,14 +636,11 @@ func sync_settings():
 	if snap_button:
 		is_snapping = snap_button.button_pressed
 
-func switch_display_mode():
+func switch_display_mode(debug_draw):
 	var spatial_editor_viewport = Utils.get_focused_spatial_editor_viewport(spatial_editor_viewports)
 	if spatial_editor_viewport:
 		var viewport = Utils.get_spatial_editor_viewport_viewport(spatial_editor_viewport)
-		if viewport.debug_draw == Viewport.DEBUG_DRAW_WIREFRAME:
-			viewport.debug_draw = 0
-		else:
-			viewport.debug_draw += 1
+		viewport.debug_draw = debug_draw
 
 # Repeatedly applying same axis will results in toggling is_global, just like pressing xx, yy or zz in blender
 func toggle_constraint_axis(axis):
---
+++ addons/gd-blender-3d-shortcuts/scenes/pie_menu/PieMenuGroup.gd
@@ -0,0 +1,136 @@
+extends Control
+const PieMenuScn = preload("PieMenu.tscn")
+
+signal item_selected(menu, index)
+
+const item4 = [
+	["Normal", 0], ["Unshaded", 1], ["Lighting", 2], ["Overdraw", 3], ["Wireframe", 4],
+	[
+		"Others",
+		[
+			["Shadows", 
+				[
+					["Shadow Atlas", 9], ["Directional Shadow Atlas", 10]
+				]
+			],
+			["SDFGI", 
+				[
+					["SDFGI", 16], ["SDFGI Probes", 17], ["GI Buffer", 18]
+				]
+			],
+			["Environment", 
+				[
+					["SSAO", 12], ["SSIL", 13]
+				]
+			],
+			["Misc", 
+				[
+					["Scene Luminance", 11], ["Disable LOD", 19], ["Cluster Reflection Probes", 23]
+				]
+			],
+		]
+	],
+	[
+		"Others2",
+		[
+			["Shadows", 
+				[
+					["Shadow Atlas", 9], ["Directional Shadow Atlas", 10]
+				]
+			],
+			["SDFGI", 
+				[
+					["SDFGI", 16], ["SDFGI Probes", 17], ["GI Buffer", 18]
+				]
+			],
+			["Environment", 
+				[
+					["SSAO", 12], ["SSIL", 13]
+				]
+			],
+			["Misc", 
+				[
+					["Scene Luminance", 11], ["Disable LOD", 19], ["Cluster Reflection Probes", 23]
+				]
+			],
+		]
+	]
+]
+
+var root
+var page_index = [0]
+
+
+# TODO: REMOVE!!!
+func _ready():
+	populate_menu(item4, PieMenuScn.instantiate())
+
+# TODO: REMOVE!!!
+func _unhandled_key_input(event):
+	if event.is_action_pressed("ui_accept"):
+		popup(get_global_mouse_position())
+
+func _on_item_selected(index):
+	if index == -1:
+		# Cancel
+		page_index = [0]
+		return
+	
+	page_index.append(index)
+	var current_menu = get_current_menu()
+	if current_menu:
+		current_menu.selected_index = -1
+		if current_menu.pie_menus.size() > 0: # Has next page
+			current_menu.popup(global_position)
+	else:
+		# Final selection, revert page index
+		if page_index.size() > 1:
+			page_index.pop_back()
+		var last_menu = get_current_menu()
+		page_index = [0]
+		emit_signal("item_selected", last_menu, index)
+
+func popup(pos):
+	global_position = pos
+	var pie_menu = get_current_menu()
+	pie_menu.popup(global_position)
+	show()
+
+func populate_menu(items, pie_menu):
+	add_child(pie_menu)
+	if not root:
+		root = pie_menu
+		root.connect("item_selected", _on_item_selected)
+
+	pie_menu.items = items
+
+	for i in items.size():
+		var item = items[i]
+		var is_array = item is Array
+		# var name = item if not is_array else item[0]
+		var value = null if not is_array else item[1]
+		if value is Array:
+			var new_pie_menu = PieMenuScn.instantiate()
+			new_pie_menu.connect("item_selected", _on_item_selected)
+			populate_menu(value, new_pie_menu)
+			pie_menu.pie_menus.append(new_pie_menu)
+		else:
+			pie_menu.pie_menus.append(null)
+	return pie_menu
+
+func clear_menu():
+	if root:
+		root.queue_free()
+
+func get_menu(indexes=[0]):
+	var pie_menu = root
+	for i in indexes.size():
+		if i == 0:
+			continue # root
+		
+		var page = indexes[i]
+		pie_menu = pie_menu.pie_menus[page]
+	return pie_menu
+
+func get_current_menu():
+	return get_menu(page_index)
